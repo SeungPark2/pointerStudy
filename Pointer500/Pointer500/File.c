0e4efacae5fda5bdfedae3f2e2295bda09629099
SeungPark2 <47220628+SeungPark2@users.noreply.github.com>
2020-09-23 21:08:56 +0900

    9/23
    16 ~ 23

---
+++ Pointer500/Pointer500/File.c
@@ -0,0 +1,22 @@
+//
+//  File.c
+//  Pointer500
+//
+//  Created by ParkSeungtae on 2020/09/23.
+//  Copyright © 2020 ParkSeungtae. All rights reserved.
+//
+
+//#include <stdio.h>
+//FILE *fi;
+//
+//int main(int argc, const char * argv[]) {
+// 
+//    if ((fi = fopen("file1.text", "r")) == NULL ) // fi = fopen("file1.text", "r"))
+//                                                  // 화일을 오픈한 결과를 fi에 대입
+//                                                  // fi == NULL 인지 체크
+//        printf("could not open file1.text \n");
+//    else
+//        printf("open file1.text \n");
+//    
+//    return 0;
+//}
