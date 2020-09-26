9615addb3917ee0180d9ca4a9e8075b3520e2c72
SeungPark2 <47220628+SeungPark2@users.noreply.github.com>
2020-09-26 09:26:28 +0900

    ExerciseProblem9_20
    ExerciseProblem9_20

--- Pointer500/Pointer500/main.c
+++ Pointer500/Pointer500/main.c
@@ -13,13 +13,14 @@
 #include "File.c"
 #include "Problem_24-30.c"
 #include "Probelm31_ExerciseProblem8.c"
+#include "ExerciseProblem9_20.c"
 
 int main(int argc, const char * argv[]) {
     
 //    basic();
 //    array();
 //    Problem24_30();
-    Probelm31_ExerciseProblem8();
+    ExerciseProblem9_20();
         
     return 0;
 }
